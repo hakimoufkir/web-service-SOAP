40485894cbe66d6076f36598204d00af86131f6a
hakimoufkir <a_oufkir@etu.enset-media.ac.ma>
2023-12-24 13:46:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<h3>Web-Service-SOAP-WSDL</h3>
+<br/>
+<img src="./images/1.png"/>
+<hr/>
+<img src="./images/2.png"/>
+<hr/>
+<img src="./images/3.png"/>
+
---
+++ src/main/java/Server/ServerJWS.java
@@ -0,0 +1,11 @@
+package Server;
+
+import jakarta.xml.ws.Endpoint;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        String url = "http://0.0.0.0:8787/";
+        Endpoint.publish(url, new BanqueService());
+        System.out.println("Wen service deploye sur :" + url );
+    }
+}
